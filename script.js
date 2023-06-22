1ef8e6b96283cc952850c54bbc194a0ee01f5632
Manikandan <122381569+Manikandan-3@users.noreply.github.com>
2023-06-22 12:23:24 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+var menuList=document.getElementById("menuList");
+menuList.style.maxHeight="0px";
+function togglemenu(){
+   if (menuList.style.maxHeight=="0px")
+   {
+      menuList.style.maxHeight="450px";
+   }
+   else
+   {
+      menuList.style.maxHeight="0px";
+   }
+}
